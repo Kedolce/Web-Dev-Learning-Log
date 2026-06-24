ebc1ca945e03b70eaebf36671d09ddc4621dbf17
Kedolce <Kedolcegocek@gmail.com>
2026-06-24 21:09:23 +0700

    Understanding again all course in Day 5 which is Advanced DOM

--- Tailwind_Learning/js/Day5.js
+++ Tailwind_Learning/js/Day5.js
@@ -1,7 +1,59 @@
-const container = document.querySelector("#container");
-const pesan = document.querySelector("#pesan");
-const btnHapus = document.querySelector("#btnHapus");
+/* Ubah judul */
+const ubahJudul = document.getElementById("ubahJudul");
+const btnUbahJudul = document.getElementById("btnUbahJudul");
 
-btnHapus.addEventListener("click", ()=>{
-    pesan.remove();
+btnUbahJudul.addEventListener("click", () => {
+    if (ubahJudul.textContent === "Saya Semakin Paham DOM") {
+    ubahJudul.textContent = "Belajar Javascript";
+    }
+    else{
+        ubahJudul.textContent = "Saya Semakin Paham DOM";
+    }
 });
+//-------------------------------------------------------------
+
+// darkmode
+const btnUbahWarnaBody = document.getElementById("btnUbahWarnaBody");
+
+btnUbahWarnaBody.addEventListener("click", () => {
+    document.body.classList.toggle("bg-black");
+    document.body.classList.toggle("text-white");
+});
+//-------------------------------------------------------------
+
+//popup
+const btnKirim = document.getElementById("btnKirim");
+const popup = document.getElementById("popup");
+const hapusPopup = document.getElementById("hapusPopup");
+
+btnKirim.addEventListener("click", () => {
+    popup.classList.remove("hidden");
+});
+
+hapusPopup.addEventListener("click", () => {
+    popup.classList.add("hidden");
+});
+//---------------------------------------------------------------
+
+//tambah pesan
+const tambahPesan = document.getElementById("tambahPesan");
+const btnTambahPesan = document.getElementById("btnTambahPesan");
+const btnHapusTerakhir = document.getElementById("btnHapusTerakhir");
+
+btnTambahPesan.addEventListener("click", () => {
+    const p = document.createElement("p");
+    p.textContent = "Halo Saya Sedang Belajar DOM";
+    tambahPesan.appendChild(p);
+})
+
+btnHapusTerakhir.addEventListener("click", () => {
+const all_p = document.querySelectorAll("#tambahPesan p");
+    const pesanTerakhir = all_p[all_p.length - 1];
+
+    if(all_p.length > 0){
+        pesanTerakhir.remove();
+    } else{
+        alert("Pesan Habis!");
+    }
+});
+//-------------------------------------------------------------
