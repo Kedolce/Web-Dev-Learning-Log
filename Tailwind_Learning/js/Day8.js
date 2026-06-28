f676a7c613c01eb84e1f8e08ff8546f4a962226f
Kedolce <Kedolcegocek@gmail.com>
2026-06-28 21:35:53 +0700

    Day 8 Data TYPE because i had fever :<

---
+++ Tailwind_Learning/js/Day8.js
@@ -0,0 +1,31 @@
+const namaKampus = "Universitas Lampung";
+const jurusan = "Teknik Elektro";
+let semester = "Otw 5";
+
+console.log(namaKampus);
+console.log(jurusan);
+console.log(semester);
+
+let angka = 10;
+
+angka = 25;
+
+console.log(angka); //25
+
+const umur = 20;
+
+umur = 30;
+
+console.log(umur); //error
+
+let saldo = 10000;
+
+console.log("Saldo awal:", saldo);
+
+saldo = saldo + 5000;
+
+console.log("Saldo sekarang:", saldo);
+
+saldo = saldo - 3000;
+
+console.log("Saldo sekarang:", saldo);
